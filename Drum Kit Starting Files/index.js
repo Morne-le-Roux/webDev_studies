c53c2c7181e022f8ba48af0b061871e2b9f78ef5
Morné le Roux <mornelerx@gmail.com>
2023-07-24 20:55:41 +0200

    misc

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,5 @@
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    new Audio("./sounds/tom-1.mp3").play();
+  });
+}
